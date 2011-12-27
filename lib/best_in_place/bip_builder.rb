a8db9265b3ffde665c1ab01b9bf483c20d863977
Paul Vonderscher <paul.vonderscher@gmail.com>
2011-12-27 21:43:46 +0100

    Adding a 'no_info' class to span tag for styling when value is nil

--- lib/best_in_place/bip_builder.rb
+++ lib/best_in_place/bip_builder.rb
@@ -44,7 +44,7 @@ module BestInPlace
         value = fieldValue ? opts[:collection][1] : opts[:collection][0]
         collection = opts[:collection].to_json
       end
-      out = "<span class='best_in_place#{opts[:type] == :no_edit ? '_no_edit' : nil}'"
+      out = "<span class='best_in_place#{opts[:type] == :no_edit ? '_no_edit' : nil} #{value.blank? ? 'no_info' : nil}'"
       out << " id='#{BestInPlace::Utils.build_best_in_place_id(@object, field)}'"
       out << " data-url='#{opts[:path].blank? ? url_for(@object) : url_for(opts[:path])}'" unless opts[:type] == :no_edit
       out << " data-object='#{@object.class.to_s.gsub("::", "_").underscore}'"
